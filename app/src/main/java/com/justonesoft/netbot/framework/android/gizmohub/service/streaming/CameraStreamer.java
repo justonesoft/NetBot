d5d99c15bf53b7db37003e98eb5d4b2c149ef7ce
justonesoft <dev.bogdan.munteanu@gmail.com>
2016-05-16 17:15:10 +0300

    CameraStreamer now reacts to commands START-STREAMING and STOP

--- app/src/main/java/com/justonesoft/netbot/framework/android/gizmohub/service/streaming/CameraStreamer.java
+++ app/src/main/java/com/justonesoft/netbot/framework/android/gizmohub/service/streaming/CameraStreamer.java
@@ -1,12 +1,10 @@
 package com.justonesoft.netbot.framework.android.gizmohub.service.streaming;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
-import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.Surface;
@@ -15,12 +13,14 @@ import android.view.WindowManager;
 
 import com.justonesoft.netbot.camera.CameraManager;
 import com.justonesoft.netbot.camera.CameraPreview;
+import com.justonesoft.netbot.framework.android.gizmohub.protocol.Message;
+import com.justonesoft.netbot.framework.android.gizmohub.protocol.MessageType;
+import com.justonesoft.netbot.framework.android.gizmohub.service.CommandListener;
 import com.justonesoft.netbot.framework.android.gizmohub.service.StreamingDataReadyListener;
+import com.justonesoft.netbot.util.Commands;
 
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -29,7 +29,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by bmunteanu on 4/15/2016.
  */
-public class CameraStreamer implements Streamer<byte[]> {
+public class CameraStreamer implements Streamer<byte[]>, CommandListener<Byte> {
     public static final int IMAGE_BUFFER_SIZE = 120000;
     private static final int DEFAULT_WIDTH = 320;
     private static final int DEFAULT_HEIGHT = 240;
@@ -40,7 +40,6 @@ public class CameraStreamer implements Streamer<byte[]> {
     private ViewGroup cameraSurface;
     private Camera camera;
     private CameraPreview cameraPreview;
-    private CameraStreamingAsyncTask cameraStreamingTask;
     private StreamingDataReadyListener dataReadyListener;
     private ByteBuffer imageSizeBuffer;
     private ByteBuffer imageBuffer;
@@ -55,13 +54,12 @@ public class CameraStreamer implements Streamer<byte[]> {
     private int jpgQuality = DEFAULT_JPG_QUALITY;
     private long lastSentFrame = 0;
 
+    private volatile boolean streaming;
+    private Thread streamingThread = null;
+
     public CameraStreamer(ViewGroup cameraSurface) {
         this.cameraSurface = cameraSurface;
-    }
-
-    @Override
-    public void setOutputStream(OutputStream outputStream) {
-        if (outputStream == null) throw new NullPointerException("Provided OutputStream is null.");
+        streaming = false;
     }
 
     @Override
@@ -79,6 +77,7 @@ public class CameraStreamer implements Streamer<byte[]> {
         fps = DEFAULT_FPS;
         jpgQuality = DEFAULT_JPG_QUALITY;
         lastSentFrame = 0;
+
         return true;
     }
 
@@ -91,11 +90,23 @@ public class CameraStreamer implements Streamer<byte[]> {
 
     @Override
     public void startStreaming() {
+        // TODO should check if prepare() has been called or move code from prepare into Constructor
+        if (streaming) return; //already streaming;
+
+        try {
+            camera.reconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
         camera.startPreview();
 
-        cameraStreamingTask = new CameraStreamingAsyncTask(this);
-
         final int twoFrameMillis = ONE_SEC_IN_MILLIS / fps;
+
+        if (streamingThread == null) {
+            streamingThread = prepareRunner();
+            streamingThread.start();
+        }
 
         camera.setPreviewCallback(new Camera.PreviewCallback() {
             @Override
@@ -105,10 +116,10 @@ public class CameraStreamer implements Streamer<byte[]> {
                     if (lastSentFrame == 0) {
                         lastSentFrame = now;
                     }
-                    Log.d("FPS", "now: "+now+" / LSF: "+lastSentFrame+" / lsf+delta: "+(lastSentFrame + twoFrameMillis));
+                    Log.d("FPS", "now: " + now + " / LSF: " + lastSentFrame + " / lsf+delta: " + (lastSentFrame + twoFrameMillis));
                     if (lastSentFrame + twoFrameMillis > now) return; // to early for desired FPS
 
-                    YuvImage image = new YuvImage(data, ImageFormat.NV21, imageWidth, imageHeight, new int[] {imageWidth, imageWidth});
+                    YuvImage image = new YuvImage(data, ImageFormat.NV21, imageWidth, imageHeight, new int[]{imageWidth, imageWidth});
 
                     baos.reset();
                     image.compressToJpeg(new Rect(0, 0, imageWidth, imageHeight), jpgQuality, baos);
@@ -123,14 +134,15 @@ public class CameraStreamer implements Streamer<byte[]> {
                 }
             }
         });
+        streaming = true;
+    }
 
-        //        cameraStreamingTask.execute((Void) null);
-
+    private Thread prepareRunner() {
         Thread runner = new Thread(new Runnable() {
             @Override
             public void run() {
                 FrameForImage ffi = null;
-                while (cameraStreamingTask != null && !cameraStreamingTask.isCancelled()) {
+                while (streaming) {
                     try {
                         ffi = streamingDeque.pollFirst(2, TimeUnit.SECONDS);
                     } catch (InterruptedException e) {
@@ -138,7 +150,7 @@ public class CameraStreamer implements Streamer<byte[]> {
                     }
                     if (ffi == null || ffi.imageData == null || ffi.imageData.length == 0) continue;
 
-                    if (cameraStreamingTask == null || cameraStreamingTask.isCancelled()) return;
+                    if (!streaming) return;
 
                     Log.i("OFFER_FRAME", "Take Frame: " + ffi.frame);
 
@@ -164,15 +176,32 @@ public class CameraStreamer implements Streamer<byte[]> {
                 }
             }
         });
-        runner.start();
+
+        return runner;
+    }
+
+    @Override
+    public void stopStreaming() {
+//        if (cameraStreamingTask != null) {
+//            cameraStreamingTask.cancel(false);
+//        }
+
+        if (camera != null) {
+            camera.stopPreview();
+            camera.setPreviewCallback(null);
+            camera.unlock();
+        }
+        streaming = false;
+        streamingThread = null;
     }
 
     @Override
     public boolean terminate() {
-        // TODO check for null
-        if (cameraStreamingTask != null) {
-            cameraStreamingTask.cancel(false);
-        }
+//        if (cameraStreamingTask != null) {
+//            cameraStreamingTask.cancel(false);
+//        }
+//        cameraStreamingTask = null;
+
         if (camera != null) {
             camera.stopPreview();
             camera.setPreviewCallback(null);
@@ -182,7 +211,6 @@ public class CameraStreamer implements Streamer<byte[]> {
 //        if (cameraPreview != null && cameraSurface != null) {
 //            cameraSurface.removeView(cameraPreview);
 //        }
-        cameraStreamingTask = null;
 
         if (baos != null) {
             try {
@@ -192,6 +220,8 @@ public class CameraStreamer implements Streamer<byte[]> {
             }
             baos = null;
         }
+
+        streamingThread = null;
         return true;
     }
 
@@ -208,11 +238,6 @@ public class CameraStreamer implements Streamer<byte[]> {
         ffi.imageData = imageData;
         boolean offered = streamingDeque.offerFirst(ffi);
         Log.i("OFFER_FRAME", "Offer Frame: " + ffi.frame + " - " + offered);
-    }
-
-    @Override
-    public void releaseStream() {
-
     }
 
     public Camera getCamera() {
@@ -246,6 +271,37 @@ public class CameraStreamer implements Streamer<byte[]> {
             result = (info.orientation - degrees + 360) % 360;
         }
         camera.setDisplayOrientation(result);
+    }
+
+    @Override
+    public boolean isInterestedIn(MessageType messageType) {
+        return MessageType.EXECUTE_COMMAND.equals(MessageType.EXECUTE_COMMAND);
+    }
+
+    @Override
+    public void dealWithMessage(Message<Byte> message) {
+        Log.d("CameraStreamer", "dealWithMessage: " + message.getPayload());
+
+        // TODO should also check if need to call prepare().
+        // For now we assume prepare is already called when registering this as Streamer in ServiceGateway.
+        if (message == null ||
+                message.getPayload() == null ||
+                (message.getPayload().byteValue() != Commands.START_IMAGE_STREAMING.getInfo() &&
+                 message.getPayload().byteValue() != Commands.STOP.getInfo()
+                )
+                )
+        {
+            return;
+        }
+
+        if (message.getPayload().byteValue() == Commands.START_IMAGE_STREAMING.getInfo()) {
+            Log.d("CameraStreamer", "Start-streaming");
+            startStreaming();
+        }
+        else {
+            Log.d("CameraStreamer", "Stop-streaming");
+            stopStreaming();
+        }
     }
 
     class FrameForImage {
